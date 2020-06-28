101a693f68fd4840f5a3cac2844c3032dece37da
ShashankJaitly <shashank16vasu@gmail.com>
2020-06-29 01:46:00 +0530

    --update add endpoint for Apiroot

--- newsletterapp/urls.py
+++ newsletterapp/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from newsletterapp import api
-
+from api.view import ApiRoot
 urlpatterns = [
+    path('', ApiRoot.as_view(), name="root")
     path('admin/', admin.site.urls),
     path('api/', include(api.edpoints)),
     path('api/auth/', include('knox.urls')),
