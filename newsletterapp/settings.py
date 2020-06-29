204cb0ad7e50444bfa4a7049d4ebdd5f541bbf8d
ShashankJaitly <shashank16vasu@gmail.com>
2020-06-29 13:13:04 +0530

    --mod settings

--- newsletterapp/settings.py
+++ newsletterapp/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'newsletterapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'newsletter',
+        'USER': 'newsletteruser',
+        'PASSWORD': 'default123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
