6408d528762b846d1ab83792c0ece345160ec652
ShashankJaitly <shashank16vasu@gmail.com>
2020-06-30 17:07:48 +0530

    --update settings aws smtp email server

--- newsletterapp/settings.py
+++ newsletterapp/settings.py
@@ -128,8 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django_ses.SESBackend'
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_SES_REGION_NAME = 'ap-south-1'
-EMAIL_HOST_USER = "shashank16jaitly@gmail.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'email-smtp.ap-south-1.amazonaws.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+
