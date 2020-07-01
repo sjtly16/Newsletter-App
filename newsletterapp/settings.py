f07e384d331dfe3edd0f86e3be921e94e47d95f0
ShashankJaitly <shashank16vasu@gmail.com>
2020-07-02 01:05:29 +0530

    --mod set debug false

--- newsletterapp/settings.py
+++ newsletterapp/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG =False
 
 ALLOWED_HOSTS = ['dsckiet-newsletter.herokuapp.com', '127.0.0.1']
 
