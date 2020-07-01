12ed5548ba3e6691e04a1ca2599e06e5ca1497b7
ShashankJaitly <shashank16vasu@gmail.com>
2020-07-01 23:41:25 +0530

    --ipdate register models with django-admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Newsletter
 
+admin.site.register(Newsletter)
+admin.site.register(CustomUser)
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
