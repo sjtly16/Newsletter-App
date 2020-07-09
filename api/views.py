2354f754fd8a6edbb2e5c67f65a346dd60986a57
ShashankJaitly <shashank16vasu@gmail.com>
2020-07-09 18:33:42 +0530

    --refactor add asynchronous funtionality

--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from django.conf import settings
 from knox.models import AuthToken
 from .serializers import LoginUserSerializer, EmailSerializer
 from .models import Newsletter
+from .tasks import send_message
 class ApiRoot(generics.ListAPIView):
     permission_classes = (permissions.AllowAny,)
 
@@ -55,37 +56,14 @@ class SendMailAPI(generics.GenericAPIView):
             html_content = request.FILES['content'].read().decode('utf-8')
             recipients = request.data.getlist('recipients')
             email_count = len(recipients)
-            if email_count>50:
-                batches = [recipients[i:i + 50] for i in range(0, email_count, 50)]
-                for batch in batches:
-                    msg = EmailMessage(subject, html_content,
-                                       from_email="team@dsckiet.com", bcc=batch, headers={
-                                           "x-priority": "1",
-                                           "x-msmail-priority": "High",
-                                        
-                                       })
-                    msg.content_subtype = "html"  # Main content is now text/html
-                    email_response = msg.send()
-                return Response({
-                    "error":False,
-                    "message": "Success",
-                    "email_response": email_response,
-                }, status=status.HTTP_200_OK)    
+            send_message.delay(email_count, recipients, html_content, subject)
+            return Response({
+                "error":False,
+                "message": "Success",
+                "email_response": "emails added to queue successfully",
+            }, status=status.HTTP_200_OK)    
 
-            else:
-                msg = EmailMessage(subject, html_content,
-                                   from_email='team@dsckiet.com', bcc=recipients, headers={
-                                       "x-priority": "1",
-                                       "x-msmail-priority": "High",
-                                       
-                                   })
-                msg.content_subtype = "html"  # Main content is now text/html
-                email_response = msg.send()
-                return Response({
-                    "error": False,
-                    "message": "Success",
-                    "email_response": email_response,
-                }, status=status.HTTP_200_OK)
+            
         else:
             return Response({
                 "error": True,
