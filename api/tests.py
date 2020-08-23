2a2bbefe2a185faeee8b97ca62224e191c7992a2
ShashankJaitly <shashank16vasu@gmail.com>
2020-08-23 19:56:50 +0530

    --refactor tests for api

--- api/tests.py
+++ api/tests.py
@@ -3,19 +3,13 @@ import os
 import uuid
 from django.urls import reverse
 from knox.models import AuthToken
-
+from rest_framework.authtoken.models import Token
 
 @pytest.fixture
 def api_client():
     from rest_framework.test import APIClient
-    return APIClient
+    return APIClient()
 
-
-@pytest.fixture
-def get_or_create_token(db, create_user):
-   user = create_user()
-   token = AuthToken.objects.create(user)[1]
-   return token
 
 @pytest.fixture
 def test_password():
@@ -29,36 +23,26 @@ def create_user(db, django_user_model, test_password):
         if 'email' not in kwargs:
            kwargs['email'] = "test@newsletterapptest.com"
         return django_user_model.objects.create_user(**kwargs)
-    return make_user
+    return make_user()
 
 @pytest.fixture
 def api_client_with_credentials(db, create_user, api_client):
-    user = create_user()
-    api_client().force_authenticate(user=user)
+    user = create_user
+    api_client.force_authenticate(user=user)
     yield api_client
-    api_client().force_authenticate(user=None)
+    api_client.force_authenticate(user=None)
 
 
 @pytest.mark.django_db
-def test_unauthorized_request(api_client):
-   url = reverse('login')
-   response = api_client().get(path=url)
-   assert response.status_code == 405
-
-@pytest.mark.django_db
-def test_login_request(api_client, get_or_create_token):
-   url = reverse('login')
-   token = get_or_create_token
-   api_client().credentials(AUTHORIZATION='Token ' + token)
-   response = api_client().get(path=url)
-   assert response.status_code == 200
-
-
-@pytest.mark.django_db
-def test_logut_request(api_client_with_credentials):
-   url = reverse('knox_logout')
-   response = api_client_with_credentials().get(path=url)
-   assert response.status_code == 200
+def test_login_request(api_client, create_user, test_password):
+    url = reverse('login')
+    user = create_user
+    post_data = {
+        "email": user.email,
+        "password": test_password
+    }
+    response = api_client.post(path=url, data=post_data)
+    assert response.status_code == 200
 
 
 def test_send_mail_request(api_client_with_credentials):
@@ -71,9 +55,9 @@ def test_send_mail_request(api_client_with_credentials):
     post_data = {
         "subject": "test-subject",
         "content": f,
-        "recipients": ["shashank.16jaitly@gmail.com", "shashank16vasu@gmail.com"]
+        "recipients": ["shashank16jaitly@gmail.com", "shashank16vasu@gmail.com"]
     }
-    response = api_client_with_credentials().post(path=url, data=post_data)
+    response = api_client_with_credentials.post(path=url, data=post_data)
     if os.path.exists(file_path):
         os.remove(file_path)
     assert response.status_code == 200
